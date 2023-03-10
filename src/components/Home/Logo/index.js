5714dcfa3b013d96f7a5110bf8a9f0b86cd9c6d9
Bruno Leite <bmarianoleite3@gmail.com>
2023-03-10 10:29:17 -0300

    bug fixes

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,3 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/logo-s.png'
 import './index.scss'
 
