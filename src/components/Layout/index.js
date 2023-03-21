08a538c50b205ef75845f0de7577ab0ff89b9603
Bruno Leite <bmarianoleite3@gmail.com>
2023-03-20 23:55:35 -0300

    toggle between light and dark mode

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,7 +4,7 @@ import './index.scss'
 
 const Layout = () => {
   return (
-    <div className="App">
+    <div className="App dark-mode">
       <Sidebar />
       <div className="page">
         <span className="tags top-tags">&lt;body&gt;</span>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import './index.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import LogoB from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -12,6 +12,7 @@ import {
   faBars,
   faClose,
   faSuitcase,
+  faCircleHalfStroke,
 } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
@@ -22,10 +23,44 @@ const Sidebar = () => {
   const { i18n } = useTranslation()
   const [showNav, setShowNav] = useState(false)
   const [navFadeOut, setNavFadeOut] = useState(false)
+  const [darkTheme, setTheme] = useState(localStorage.getItem('darkTheme'))
 
   function changeLanguage(e) {
     i18n.changeLanguage(e.target.value)
   }
+
+  function changeTheme() {
+    setTheme(!darkTheme)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('darkTheme', darkTheme)
+
+    function setThemeColors() {
+      const darkMode = {
+        '--main-color': '#0093b9',
+        '--secondary-color': '#e9f1fa',
+        '--tertiary-color': '#fff',
+        '--quaternary-color': '#000',
+        '--sidebar-links': '#fff',
+        '--is-dark-theme': 1,
+      }
+      const lightMode = {
+        '--main-color': '#0093b9',
+        '--secondary-color': '#1d1d1d',
+        '--tertiary-color': '#4d4d4e',
+        '--quaternary-color': '#e9f1fa',
+        '--sidebar-links': '#fff',
+        '--is-dark-theme': 0,
+      }
+      const currentTheme = darkTheme ? darkMode : lightMode
+
+      for (var prop in currentTheme) {
+        document.documentElement.style.setProperty(prop, currentTheme[prop])
+      }
+    }
+    setThemeColors()
+  }, [darkTheme])
 
   return (
     <div className="nav-bar">
@@ -133,7 +168,12 @@ const Sidebar = () => {
           </a>
         </li>
       </ul>
-      <ul className="language-icons">
+      <ul className="toggle-buttons">
+        <li>
+          <button onClick={changeTheme} className="theme-button">
+            <FontAwesomeIcon icon={faCircleHalfStroke} />
+          </button>
+        </li>
         <li>
           <button onClick={changeLanguage} value="en">
             <GB title="English" className="..." />
@@ -145,18 +185,21 @@ const Sidebar = () => {
           </button>
         </li>
       </ul>
-      <div className="mobile-language">
+      <div className="toggle-buttons-mobile">
         <button onClick={changeLanguage} value="en">
           <GB title="English" className="..." />
         </button>
         <button onClick={changeLanguage} value="pt">
           <BR title="Portuguese" className="..." />
         </button>
+        <button onClick={changeTheme} className="theme-button">
+          <FontAwesomeIcon icon={faCircleHalfStroke} />
+        </button>
       </div>
       <FontAwesomeIcon
         onClick={() => setShowNav(true)}
         icon={faBars}
-        color="#0093B9"
+        color="var(--main-color)"
         size="3x"
         className="hamburger-icon"
       />
