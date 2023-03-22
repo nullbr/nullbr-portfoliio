6a26de1bb58b6612a4938edb770128d49ce3ffd5
Bruno Leite <bmarianoleite3@gmail.com>
2023-03-22 20:13:11 -0300

    adding repo url and adapting to mobile

--- src/components/Dashboard/home.js
+++ src/components/Dashboard/home.js
@@ -1,80 +1,98 @@
-import { useRef } from 'react';
-import { auth, storage, db } from '../../firebase';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { doc, setDoc } from 'firebase/firestore';
-
+import { useRef } from 'react'
+import { auth, storage, db } from '../../firebase'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { doc, setDoc } from 'firebase/firestore'
 
 const Home = () => {
-    const form = useRef();
+  const form = useRef()
 
-    const submitPortfolio = (e) => {
-        e.preventDefault();
-        const name = form.current[0]?.value;
-        const description_en = form.current[1]?.value;
-        const description_pt = form.current[2]?.value;
-        const url = form.current[3]?.value;
-        const image = form.current[4]?.files[0];
+  const submitPortfolio = (e) => {
+    e.preventDefault()
+    const name = form.current[0]?.value
+    const description_en = form.current[1]?.value
+    const description_pt = form.current[2]?.value
+    const url = form.current[3]?.value
+    const repo_url = form.current[4]?.value
+    const image = form.current[5]?.files[0]
 
-        const storageRef = ref(storage, `portfolio/${image.name}`);
+    const storageRef = ref(storage, `portfolio/${image.name}`)
 
-        uploadBytes(storageRef, image).then(
-            (snapshot) => {
-                getDownloadURL(snapshot.ref).then((downloadUrl) => {
-                    savePortfolio({
-                        name,
-                        description_en,
-                        description_pt,
-                        url,
-                        image: downloadUrl
-                    })
-                }, (error) => {
-                    console.log(error);
-                    savePortfolio({
-                        name,
-                        description_en,
-                        description_pt,
-                        url,
-                        image: null
-                    })
-                })
-            }, (error) => {
-                console.log(error);
-                savePortfolio({
-                    name,
-                    description_en,
-                    description_pt,
-                    url,
-                    image: null
-                })
-            }
+    uploadBytes(storageRef, image).then(
+      (snapshot) => {
+        getDownloadURL(snapshot.ref).then(
+          (downloadUrl) => {
+            savePortfolio({
+              name,
+              description_en,
+              description_pt,
+              url,
+              repo_url,
+              image: downloadUrl,
+            })
+          },
+          (error) => {
+            console.log(error)
+            savePortfolio({
+              name,
+              description_en,
+              description_pt,
+              url,
+              repo_url,
+              image: null,
+            })
+          }
         )
-    }
-
-    const savePortfolio = async (portfolio) => {
-        try {
-            await setDoc(doc(db, 'portfolio', portfolio.name), portfolio);
-            window.location.reload(false);
-        } catch (error) {
-            console.log(error);
-            alert('Failed to add portfolio');
-        }
-    }
-
-    return (
-        <div className="container dashboard-page">
-            <p>Testing CI v2</p>
-
-            <form ref={form} onSubmit={submitPortfolio}>
-                <p><input type="text" placeholder="Name" /></p>
-                <p><textarea placeholder="Description English" /></p>
-                <p><textarea placeholder="Description Portuguese" /></p>
-                <p><input type="text" placeholder="Url" /></p>
-                <p><input type="file" placeholder="Image" /></p>
-                <button type="submit">Submit</button>
-                <button onClick={() => auth.signOut()}>Sign out</button>
-            </form>
-        </div>
+      },
+      (error) => {
+        console.log(error)
+        savePortfolio({
+          name,
+          description_en,
+          description_pt,
+          url,
+          repo_url,
+          image: null,
+        })
+      }
     )
+  }
+
+  const savePortfolio = async (portfolio) => {
+    try {
+      await setDoc(doc(db, 'portfolio', portfolio.name), portfolio)
+      window.location.reload(false)
+    } catch (error) {
+      console.log(error)
+      alert('Failed to add portfolio')
+    }
+  }
+
+  return (
+    <div className="container dashboard-page">
+      <form ref={form} onSubmit={submitPortfolio}>
+        <p>
+          <input type="text" placeholder="Name" />
+        </p>
+        <p>
+          <textarea placeholder="Description English" />
+        </p>
+        <p>
+          <textarea placeholder="Description Portuguese" />
+        </p>
+        <p>
+          <input type="text" placeholder="Url" />
+        </p>
+        <p>
+          <input type="text" placeholder="GitHub" />
+        </p>
+        <p>
+          <input type="file" placeholder="Image" />
+        </p>
+        <button type="submit">Submit</button>
+        <button onClick={() => auth.signOut()}>Sign out</button>
+      </form>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,95 +1,102 @@
-import React, { useEffect, useState } from "react";
-import Loader from "react-loaders";
-import AnimatedLetters from "../AnimatedLetters";
-import "./index.scss";
-import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../../firebase';
-import { useTranslation } from "react-i18next";
+import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../../firebase'
+import { useTranslation } from 'react-i18next'
 
-const Portfolio = () => { 
-    const [letterClass, setLetterClass] = useState('text-animate');
-    const [portfolio, setPortfolio] = useState([]);
-    const { t } = useTranslation();
-    const { i18n } = useTranslation();
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const [portfolio, setPortfolio] = useState([])
+  const { t } = useTranslation()
+  const { i18n } = useTranslation()
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
 
-        return () => {
-            clearTimeout(timer);
-        }
-    });
-
-    useEffect(() => {
-        getPortfolio();
-    }, []);
-
-    const getPortfolio = async () => {
-        const querySnapshot = await getDocs(collection(db, 'portfolio'));
-        setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
+    return () => {
+      clearTimeout(timer)
     }
+  })
 
-    const renderPortfolio = (portfolio) => {
-        return (
-            <div className="images-container">
-                {
-                    portfolio.map((port, idx) => {
-                        let description = i18n.language === 'en' ? port.description_en : port.description_pt 
+  useEffect(() => {
+    getPortfolio()
+  }, [])
 
-                        return (
-                            <a
-                                className="image-box"
-                                href={port.url}
-                                target="_blank"
-                                rel="noreferrer"
-                                key={idx}
-                            >
-                                <div>
-                                    <img 
-                                    src={port.image}
-                                    className="portfolio-image"
-                                    alt="portfolio" />
-                                    <div className="content">
-                                        <p className="title">{port.name}</p>
-                                        <h4 className="description">{description}</h4>
-                                        <button
-                                            className="btn"
-                                            onClick={() => window.open(port.url)}
-                                        >View</button>
-                                    </div>
-                                </div>
-                            </a>
-                        )
-                    })
-                }
-            </div>
-        );
-    }
+  const getPortfolio = async () => {
+    const querySnapshot = await getDocs(collection(db, 'portfolio'))
+    setPortfolio(querySnapshot.docs.map((doc) => doc.data()))
+  }
 
-
+  const renderPortfolio = (portfolio) => {
     return (
-        <>
-            <div className="container portfolio-page">
-                <div className="content-zone">
-                    <h1 className="page-title">
-                        <AnimatedLetters
-                            letterClass={letterClass}
-                            strArray={t('portfolio.title').split('')}
-                            idx={15}
-                        />
-                    </h1>
-                    <p>
-                        {t('portfolio.paragraph')}
-                    </p>
-                    <br />
-                    <div>{renderPortfolio(portfolio)}</div>
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          let description =
+            i18n.language === 'en' ? port.description_en : port.description_pt
+
+          return (
+            <a
+              className="image-box"
+              href={port.url || port.repo_url}
+              target="_blank"
+              rel="noreferrer"
+              key={idx}
+            >
+              <div>
+                <img
+                  src={port.image}
+                  className="portfolio-image"
+                  alt="portfolio"
+                />
+                <div className="content">
+                  <p className="title">{port.name}</p>
+                  <h4 className="description">{description}</h4>
+                  <button
+                    hidden={port.url ? '' : 'hidden'}
+                    className="btn"
+                    onClick={() => window.open(port.url)}
+                  >
+                    {t('portfolio.view')}
+                  </button>
+                  <button
+                    hidden={port.repo_url ? '' : 'hidden'}
+                    className="btn"
+                    onClick={() => window.open(port.repo_url)}
+                  >
+                    {t('portfolio.github')}
+                  </button>
                 </div>
-            </div>
-            <Loader type="pacman" />
-        </>
-    );
+              </div>
+            </a>
+          )
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <div className="content-zone">
+          <h1 className="page-title">
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={t('portfolio.title').split('')}
+              idx={15}
+            />
+          </h1>
+          <p>{t('portfolio.paragraph')}</p>
+          <br />
+          <div>{renderPortfolio(portfolio)}</div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
-export default Portfolio;
+export default Portfolio
