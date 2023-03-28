9586f6710e9c1ec214bdd44c7cc5e5665634b290
Bruno Leite <bmarianoleite3@gmail.com>
2023-03-28 20:39:50 -0300

    Refactor toggle buttons code

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,8 @@
 import './index.scss'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import LogoB from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import GB from 'country-flag-icons/react/3x2/GB'
-import BR from 'country-flag-icons/react/3x2/BR'
 import {
   faHome,
   faUser,
@@ -12,47 +10,16 @@ import {
   faBars,
   faClose,
   faSuitcase,
-  faSun,
-  faMoon,
 } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { useAppContext } from '../../context'
+import ToggleButtons from './ToggleButtons'
 
 const Sidebar = () => {
   const [showNav, setShowNav] = useState(false)
   const [navFadeOut, setNavFadeOut] = useState(false)
-  const { t, i18n } = useAppContext()
-  const [language, setLanguage] = useState(localStorage.getItem('language'))
-
-  const changeLanguage = (e) => {
-    setLanguage(e.target.value)
-    i18n.changeLanguage(e.target.value)
-  }
-
-  useEffect(() => {
-    localStorage.setItem('language', language)
-  }, [language])
-
-  const getTheme = () => {
-    const storage = localStorage.getItem('theme')
-
-    if (storage !== null) {
-      return localStorage.getItem('theme')
-    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark'
-    } else {
-      return 'light'
-    }
-  }
-  const [theme, setTheme] = useState(getTheme())
-
-  useEffect(() => {
-    localStorage.setItem('theme', theme)
-
-    const root = document.documentElement
-    root.className = theme
-  }, [theme])
+  const { t } = useAppContext()
 
   return (
     <div className="nav-bar">
@@ -160,40 +127,9 @@ const Sidebar = () => {
           </a>
         </li>
       </ul>
-      <ul className="toggle-buttons">
-        <li>
-          <button
-            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            className="theme-button"
-          >
-            <FontAwesomeIcon icon={faSun} />
-          </button>
-        </li>
-        <li>
-          <button onClick={changeLanguage} value="en">
-            <GB title="English" className="..." />
-          </button>
-        </li>
-        <li>
-          <button onClick={changeLanguage} value="pt">
-            <BR title="Portuguese" className="..." />
-          </button>
-        </li>
-      </ul>
-      <div className="toggle-buttons-mobile">
-        <button onClick={changeLanguage} value="en">
-          <GB title="English" className="..." />
-        </button>
-        <button onClick={changeLanguage} value="pt">
-          <BR title="Portuguese" className="..." />
-        </button>
-        <button
-          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-          className="theme-button"
-        >
-          <FontAwesomeIcon icon={theme === 'dark' ? faSun : faMoon} />
-        </button>
-      </div>
+
+      <ToggleButtons />
+
       <FontAwesomeIcon
         onClick={() => setShowNav(true)}
         icon={faBars}
--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const AppContext = createContext()
@@ -9,8 +9,29 @@ const GlobalContext = ({ children }) => {
   const { t } = useTranslation()
   const { i18n } = useTranslation()
 
+  const defaultTheme = useMemo(() => {
+    const storage = localStorage.getItem('theme')
+
+    if (storage === 'light' || storage === 'dark') {
+      return localStorage.getItem('theme')
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark'
+    } else {
+      return 'light'
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('theme', defaultTheme)
+
+    const root = document.documentElement
+    root.className = defaultTheme
+  }, [defaultTheme])
+
   return (
-    <AppContext.Provider value={{ t, i18n }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ t, i18n, defaultTheme }}>
+      {children}
+    </AppContext.Provider>
   )
 }
 
